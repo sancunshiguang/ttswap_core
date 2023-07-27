ae3c15ef770aec23f7e6eaf8435045bbf1d4c4ee
sancunshiguang <lq_feng@126.com>
2023-07-27 15:57:02 +0800

    修改管理员状态引用

--- Contracts/interfaces/ITTSwapV1Market.sol
+++ Contracts/interfaces/ITTSwapV1Market.sol
@@ -5,7 +5,7 @@ import "./market/ITTSwapV1MarketImmutables.sol";
 import "./market/ITTSwapV1MarketState.sol";
 import "./market/ITTSwapV1MarketDerivedState.sol";
 import "./market/ITTSwapV1MarketCreatorActions.sol";
-import "./market/ITTSwapV1MarketManagerActions.sol";
+import "./market/ITTSwapV1MarketorActions.sol";
 import "./market/ITTSwapV1MarketGatorActions.sol";
 import "./market/ITTSwapV1MarketEvents.sol";
 
@@ -14,7 +14,7 @@ interface ITTSwapV1Market is
     ITTSwapV1MarketState,
     ITTSwapV1MarketDerivedState,
     ITTSwapV1MarketCreatorActions,
-    ITTSwapV1MarketManagerActions,
+    ITTSwapV1MarketorActions,
     ITTSwapV1MarketGatorActions,
     ITTSwapV1MarketEvents
 {}
